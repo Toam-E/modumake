2b9c44cab8c0d03d65055a83fba1d0322c339a0f
Toam <94446731+Toam-E@users.noreply.github.com>
2025-07-05 17:10:13 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/modumake-robo-designer-x/',
+  base: process.env.DEPLOY_ENV === 'github' ? '/modumake-robo-designer-x/' : '/',
   server: {
     host: "::",
     port: 8080,
