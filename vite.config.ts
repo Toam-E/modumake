813855d757e7f8f9ae32eeefe9de6198f3728323
Toam <elharar.toam@campus.technion.ac.il>
2025-07-05 14:59:06 +0300

    new build

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/modumake-robo-designer-x/',
   server: {
     host: "::",
     port: 8080,
